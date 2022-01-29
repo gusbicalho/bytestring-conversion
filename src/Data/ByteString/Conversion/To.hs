d2bd066a3118641a9e15d7b49af47c4d61428ac3
Gustavo Bicalho <gusbicalho@gmail.com>
2022-01-29 19:36:07 +0000

    replace imports to deprecated/removed bytestring modules

--- src/Data/ByteString/Conversion/To.hs
+++ src/Data/ByteString/Conversion/To.hs
@@ -16,12 +16,11 @@ module Data.ByteString.Conversion.To
 
 import Data.ByteString (ByteString)
 import Data.ByteString.Conversion.Internal
-import Data.ByteString.Lazy.Builder
-import Data.ByteString.Lazy.Builder.Extras hiding (runBuilder)
+import Data.ByteString.Builder
+import Data.ByteString.Builder.Extra hiding (runBuilder)
 import Data.CaseInsensitive (CI, original)
 import Data.Int
 import Data.List (intersperse)
-import Data.Monoid
 import Data.Text (Text)
 import Data.Text.Encoding (encodeUtf8)
 import Data.Word
